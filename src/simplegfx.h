d51c792d1a54673c5bfa0b52821022bfc75d9861
Zane <zanegloh@gmail.com>
2023-01-02 15:23:34 -0800

    added pixel and canvas structs

--- src/simplegfx.h
+++ src/simplegfx.h
@@ -3,6 +3,16 @@
 
 #include <stdbool.h>
 
+struct pixel {
+    uint32_t color;
+};
+
+struct canvas_data {
+    int x_dim;
+    int y_dim;
+    struct pixel **canvas;
+};
+
 bool simplegfx_create_img(int x_dim, int y_dim);
 
 #endif /* SIMPLEGFX_H_ */
