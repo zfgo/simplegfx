d778888387616636ad26a6aa7902a431a031dca4
Zane <zanegloh@gmail.com>
2023-01-04 10:45:50 -0800

    return a simplegfx_canvas instead of a pointer to a simplegfx_canvas

--- src/simplegfx.c
+++ src/simplegfx.c
@@ -2,7 +2,7 @@
 
 #define UNUSED __attribute__((unused))
 
-const Simplegfx_canvas *simplegfx_create_canvas(int x_dim, int y_dim) {
+const Simplegfx_canvas simplegfx_create_canvas(int x_dim, int y_dim) {
     /*
      * Create an array of memory ("pixels") and fill each pixel with
      * different colors, etc. This library on creates this array of 
@@ -15,5 +15,5 @@ const Simplegfx_canvas *simplegfx_create_canvas(int x_dim, int y_dim) {
     canvas.x_dim = x_dim;
     canvas.y_dim = y_dim;
 
-    return &canvas;
+    return canvas;
 } 
--- src/simplegfx.h
+++ src/simplegfx.h
@@ -11,6 +11,6 @@ typedef struct simplegfx_canvas {
     uint32_t *pixels;
 } Simplegfx_canvas;
 
-const Simplegfx_canvas *simplegfx_create_canvas(int x_dim, int y_dim);
+const Simplegfx_canvas simplegfx_create_canvas(int x_dim, int y_dim);
 
 #endif /* SIMPLEGFX_H_ */
