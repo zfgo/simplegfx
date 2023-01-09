89f0505f2ba6a1ffbb2c1d9078f9a8501984cb17
Zane <zanegloh@gmail.com>
2023-01-09 15:23:44 -0800

    added comments; commented out (*create) fxn

--- src/simplegfx.h
+++ src/simplegfx.h
@@ -1,15 +1,22 @@
 #ifndef SIMPLEGFX_H_
 #define SIMPLEGFX_H_
 
+/* interface definition for the simplegfx graphics library
+ *
+ * patterned roughly after Joseph Sventek's data structures as seen in
+ * "C and Data Structures"
+ */
 #include <stdbool.h>
-#include <stdint.h>
-#include <limits.h>
+#include <stdint.h>                         /* needed for uint32_t */
 
-#define DEFAULT_DIM 1000
+#define DEFAULT_DIM 1000                    /* set default dimensions */
 
 
-typedef struct simplegfx_canvas Simplegfx_canvas;
+typedef struct simplegfx_canvas Simplegfx_canvas;   /* forward reference */
 
+/* Create canvas function
+ */
+const Simplegfx_canvas *Simplegfx_canvas_create(int x_dimension, int y_dimension);
 
 struct simplegfx_canvas {
     /* The private data of the canvas 
@@ -18,12 +25,11 @@ struct simplegfx_canvas {
     
     /* create a new canvas
      */
-    const Simplegfx_canvas *(*create)(const Simplegfx_canvas *canvas, int x_dim, int y_dim);
+    //const Simplegfx_canvas *(*create)(int x_dimension, int y_dimension);
 
     /* destroy the canvas
      */
     void (*destroy)(const Simplegfx_canvas *canvas);
 };
-
 
 #endif /* SIMPLEGFX_H_ */
