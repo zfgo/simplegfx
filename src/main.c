31344916d6fe3c930958135515ca99151d97afa9
Zane Globus-O'Harra <zfg@uoregon.edu>
2023-01-10 09:40:28 -0800

    include test of fill_canvas

--- src/main.c
+++ src/main.c
@@ -8,6 +8,7 @@ int main(void) {
     if ((canvas = Simplegfx_canvas_create(10,10)) == NULL) {
         printf("unable to make canvas\n");
     }
+    canvas->fill_canvas(canvas, 255, 0, 0);
     canvas->write(canvas, "./tests/test.ppm");
     canvas->destroy(canvas);
     printf("Done.\n");
