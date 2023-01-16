4be89ea5eeaeff8e30873f41d5ea31e3a805969c
Zane Globus-O'Harra <zanegloh@gmail.com>
2023-01-16 10:17:20 -0800

    added test for assign_pixel

--- src/main.c
+++ src/main.c
@@ -5,10 +5,11 @@
 int main(void) {
     const Simplegfx_canvas *canvas = NULL;
 
-    if ((canvas = Simplegfx_canvas_create(10,10)) == NULL) {
+    if ((canvas = Simplegfx_canvas_create(100,100)) == NULL) {
         printf("unable to make canvas\n");
     }
     canvas->fill_canvas(canvas, 255, 0, 0, 125);
+    canvas->assign_pixel(canvas, 1, 1, 0, 0, 0, 255);
     canvas->write(canvas, "./tests/test.ppm");
     canvas->destroy(canvas);
     printf("Done.\n");
